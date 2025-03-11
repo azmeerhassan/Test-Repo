a3f4840cadfdea8ee347184f54eb535efd66f765
Azmeer Hassan <azmeerhassanammad@gmail.com>
2025-03-11 11:11:18 +0500

    for-each

--- JS practice/script.js
+++ JS practice/script.js
@@ -1,9 +1,5 @@
 let array = [6, 'CSE', 'BE', 'Second', {gpa: 3.3}];
-console.log(array);
-console.log(array[0]);
-console.log(array[1]);
-console.log(array[2]);
-console.log(array[3]);
-console.log(array[4]);
-console.log(array[4].gpa);
-console.log(array[5]);
+array.forEach(function(arr)
+{
+    console.log(arr);
+})
