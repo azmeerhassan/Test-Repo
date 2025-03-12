ea4b0f7aa3b1408ea87698a56985167d12e0a9ec
Azmeer Hassan <azmeerhassanammad@gmail.com>
2025-03-12 09:06:52 +0500

    Fetch API

--- JS practice/script.js
+++ JS practice/script.js
@@ -1,10 +1,6 @@
-const arr1 = [1, 2, 3];
-const arr2 = [arr1];
-
-console.log(arr1);
-console.log(arr1.length);
-console.log(arr2);
-console.log(arr2.length);
+fetch('https://google.com')
+.then(res => res.text())
+.then(data => console.log(data));
 
 
 
