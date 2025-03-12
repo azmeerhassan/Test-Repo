11091d0889b59aeb45e37706780737792b422a56
Azmeer Hassan <azmeerhassanammad@gmail.com>
2025-03-12 08:51:39 +0500

    Spread Operator

--- JS practice/script.js
+++ JS practice/script.js
@@ -1,25 +1,10 @@
-function sum(a, b)
-{
-    return a+b;
-}
+const arr1 = [1, 2, 3];
+const arr2 = [arr1];
 
-let Sum = function(a, b)
-{
-    return a+b;
-}
-let sum1 = (a, b) =>
-{
-    return a+b;
-}
-
-let sum2 = (a, b) => a+b;
-
-let square = a => a*a;
-
-console.log(Sum(3, 3));
-console.log(sum1(3, 3));
-console.log(sum2(3, 3));
-console.log(square(3));
+console.log(arr1);
+console.log(arr1.length);
+console.log(arr2);
+console.log(arr2.length);
 
 
 
