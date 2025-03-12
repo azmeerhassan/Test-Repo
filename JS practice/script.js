c1c9b4f8f87a97f43fae3f0bfb6be0d0f322264c
Azmeer Hassan <azmeerhassanammad@gmail.com>
2025-03-12 09:17:35 +0500

    Fetch API & Async Await

--- JS practice/script.js
+++ JS practice/script.js
@@ -1,6 +1,14 @@
-fetch('https://google.com')
-.then(res => res.text())
-.then(data => console.log(data));
+// fetch('https://google.com')
+// .then(res => res.text())
+// .then(data => console.log(data));
+async function fetchData()
+{
+    let res = await fetch('https://google.com');
+    let data =  await res.text();
+    console.log(data);   
+}
+fetchData('https://google.com');
+
 
 
 
