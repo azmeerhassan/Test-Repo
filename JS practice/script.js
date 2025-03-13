66e073ad8ebdeabd449ba633cdbd233b5e7b15b3
Azmeer Hassan <azmeerhassanammad@gmail.com>
2025-03-13 13:00:33 +0500

    Callback

--- JS practice/script.js
+++ JS practice/script.js
@@ -1,11 +1,20 @@
-function greeting (name)
-{
-    console.log("AOA, "+name);
-}
+// function greeting (name)
+// {
+//     console.log("AOA, "+name);
+// }
 
-function processUserInput(callback)
-{
-    var name = prompt("Please enter your name.");
-    callback(name);
-}
-processUserInput(greeting);
+// function processUserInput(callback)
+// {
+//     var name = prompt("Please enter your name.");
+//     callback(name);
+// }
+// processUserInput(greeting);
+
+let stocks = 
+{ 
+    Fruits:["strawberry", "grapes", "banana", "apple"], 
+    liquid:["water", "ice"], 
+    holder:["cone", "cup", "stick"], 
+    toppings:["chocolate", "peanuts"], 
+};
+
