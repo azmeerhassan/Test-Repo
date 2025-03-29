74273d874392121ee47c653fcb4484d84aa9ea51
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-03-30 01:54:32 +0500

    fs.writeFile()

--- JS practice/app.js
+++ JS practice/app.js
@@ -1,11 +1,13 @@
 const { log } = require('console');
 let fs = require('fs');
 
-fs.readFile('calc.js', 'utf8', (err, data)=>{
+fs.writeFile('calc1.js', 'console.log("writeFile")', (err, data)=>{
     if(err)
         console.log(err);
         
     else
     console.log(data);
+    console.log("Done");
+    
     
 })
