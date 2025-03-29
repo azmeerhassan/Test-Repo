7792003403d0346602c2f20dd812f522db1b2491
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-03-29 18:26:50 +0500

    installed express

--- JS practice/app.js
+++ JS practice/app.js
@@ -3,6 +3,6 @@ var http = require('http')
 http.createServer((req, res)=>
 {
     res.writeHead(200, {'Content-Type':'text/html'})
-    res.write("Hello World!")
-    res.end()
+    res.end("Hello World!")
+    
 }).listen(8080)
