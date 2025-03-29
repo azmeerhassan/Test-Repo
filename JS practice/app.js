b665afcbc417e0b45c5ddbf19a2ad4fe3f49b2e2
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-03-30 01:59:33 +0500

    appendFile()

--- JS practice/app.js
+++ JS practice/app.js
@@ -1,13 +1,6 @@
-const { log } = require('console');
 let fs = require('fs');
 
-fs.appendFile('calc.js', 'console.log("add done")', (err, data)=>{
-    if(err)
-        console.log(err);
-        
-    else
-    console.log(data);
-    console.log("Done");
-    
+fs.unlink('calc1.js', (data)=>{
+    console.log("deleted");
     
 })
