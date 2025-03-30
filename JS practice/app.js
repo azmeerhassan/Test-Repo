482bcd5e9b87a28032bd0f6209f15c2f6d89d871
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-03-30 20:11:06 +0500

    used query id and placeholder

--- JS practice/app.js
+++ JS practice/app.js
@@ -6,6 +6,16 @@ app.get('/', (req, res)=>{
     res.send('Hello World');
     
 })
+//query id 
+app.get('/azmeer', (req, res)=>{
+    const id = req.query.id
+    res.send('welcome back azmeer' + id)
+})
+//placeholder
+app.get('/azmeer/:id', (req,res)=>{
+    const id = req.params.id
+    res.send('Hey Azmeer '+id)
+})
 
 app.listen(9000, ()=>{
     console.log('running');
